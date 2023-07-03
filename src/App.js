4fb7cd052e8e9df2d174f7a46485ade3ed3bd810
Jason Alberto <alberto.jason@gmail.com>
2023-07-03 16:32:40 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const dummyIdeas = [
+    { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+
+    </main>
   );
 }
 
